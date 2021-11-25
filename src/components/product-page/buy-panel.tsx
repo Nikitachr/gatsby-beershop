754eb143c2cdb0cea89356bbcefdb5a087400c06
Nikita Chernega <60040399+Nikitachr@users.noreply.github.com>
2021-11-26 00:45:32 +0200

    create product pages

--- src/components/product-page/buy-panel.tsx
+++ src/components/product-page/buy-panel.tsx
@@ -9,7 +9,22 @@ type TBuyPanelProps = {
 const BuyPanel: FC<TBuyPanelProps> = ({ product }) => {
     return (
         <div>
-            <h1 className="font-bold text-xl">{product.title}</h1>
+            <div className="pb-4 border-b-2 border-secondary">
+                <h1 className="font-bold text-2xl">{product.title}</h1>
+                <span className="font-bold text-blue text-2xl">{`$${product.price.toFixed(2)}`}</span>
+            </div>
+            <div className="pt-4">
+                <div className="flex items-center gap-10">
+                    <div className="flex gap-4">
+                        <span className="font-bold">SIZE</span>
+                        <span className="">{`${product.size}ml`}</span>
+                    </div>
+                    <div className="flex gap-4">
+                        <span className="font-bold">ALC</span>
+                        <span className="">{`${product.alcohol}%`}</span>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
